60c2d4680a7516169f950935c80745c1d8bbb377
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2019-07-21 22:32:18 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,18 @@
+print("hello world");
+hello world
+------------------------
+msg="Hello World"
+print(msg)
+Hello World
+------------------------
+firstname="yash"
+lastname="wanth"
+fullname=firstname+''+lastname;
+print(fullname)
+yashwanth
+-------------------------
+a=int(input("Enter the value of a:"));
+b=int(input("Enter the value of b:"));
+c=a+b;
+print("The value is",c)
+--------------------------
